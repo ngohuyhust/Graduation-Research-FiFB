843af9c1009b1344f419dd9de81bfe40861a7ae1
ngohuyhust <lagvay@gmail.com>
2026-05-27 21:44:23 +0700

    doc

---
+++ Tools/sync-gr.ps1
@@ -0,0 +1,7 @@
+Write-Host "Syncing GR documents from Google Drive..."
+
+rclone sync "GraduationResearch:GR" "../Document" `
+  --drive-export-formats txt `
+  --progress
+
+Write-Host "Done. GR documents synced to GR"
